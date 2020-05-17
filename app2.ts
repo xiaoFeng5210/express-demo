84a69fff6e1320ee5381adc9b63e11a3b359ff3b
qingfengzhang <804875728@qq.com>
2020-05-17 13:08:43 +0800

    use 中间件

--- app2.ts
+++ app2.ts
@@ -14,7 +14,11 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+// 中间件
+app.use(function (req, res, next) {
+  console.log(req.path);
+  next();
+} as RequestHandler);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
